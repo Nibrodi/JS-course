9e93e361f0a6e6eb8351aa2e2f1471c5464dece3
«Katerina <nibrodi@llist.ru>
2019-10-09 17:32:59 +0300

    new idea

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -14,16 +14,14 @@ console.log( b + y );
 let li = [1, 2, 3, 4, 5, 6, 7, 8];
 li.splice(3, 3);
 console.log( li );
-li[5] = 10;
-li[6] = 5e+9;
+li.push(10, 5e+9);
 console.log( li );
 
 let ob = { numbers: ''};
 ob.numbers = li.join( '; ' );
 console.log(ob);
 
-// ob += {  newNumbers: li + li[5] + li[5]}
-li[7] = li[5];
-li[8] = li[5];
+li[7] = li[4];
+li[8] = li[4];
 ob.newNumbers = li.join( '; ' );
 console.log(ob);
