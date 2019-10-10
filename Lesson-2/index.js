596edefb9bc29900e81d9920cd9a7b995a78b4b7
«Katerina <nibrodi@llist.ru>
2019-10-10 16:55:02 +0300

    new correct

--- Lesson-2/index.js
+++ Lesson-2/index.js
@@ -1,5 +1,5 @@
-let fedorPoints = 15;
-let petrPoints = 19;
+let fedorPoints = 20; //введите значения очков Федора
+let petrPoints = 19; //введите значение очков Петра
 
 if ( fedorPoints <= 21 && !( fedorPoints > 21) && ( fedorPoints > petrPoints) && !(fedorPoints === petrPoints) ) {
     console.log("Федя выиграл со счетом " + fedorPoints + "!!")
